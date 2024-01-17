9ec3b022d51a0c41d8f8d5fb90ca55cedd4a89ec
Gustavo Costa <guucoostaa@hotmail.com>
2024-01-17 16:48:25 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gcost4/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gcost4.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gcost4.workshopmongo.domain.Post;
+import com.gcost4.workshopmongo.repository.PostRepository;
+import com.gcost4.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
